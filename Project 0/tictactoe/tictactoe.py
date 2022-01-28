a4413c0f93a41660e8a21b6b5356556d7fc2891e
tentato <alepaw99@gmail.com>
2022-01-28 23:27:15 +0100

    terminal-function-done

--- Project 0/tictactoe/tictactoe.py
+++ Project 0/tictactoe/tictactoe.py
@@ -70,58 +70,29 @@ def terminal(board):
     Returns True if game is over, False otherwise.
     """
     # check ROWS
-    # for i in range(3):
-    #     if board[i][0] == board[i][1] == board[i][2] == X:
+    for i in range(3):
+        if (board[i][0] == board[i][1] == board[i][2] == X) or (board[i][0] == board[i][1] == board[i][2] == O):
+            return True
 
+    # check COLUMNS
+    for j in range(3):
+        if (board[0][j] == board[1][j] == board[2][j] == X) or (board[0][j] == board[1][j] == board[2][j] == O):
+            return True
 
-    # for i in range(3):
-    #     sum = 0
-    #     for j in range(3):
-    #         if board[i][j] == "X":
-    #             sum += 1
-    #         elif board[i][j] == "O":
-    #             sum -= 1
+    # check diagonal
+    if (board[0][0] == board[1][1] == board[2][2] == X) or (board[0][0] == board[1][1] == board[2][2] == O):
+        return True
 
-    #     if sum == 3 or sum == -3:
-    #         return True
+    # check second diagonal
+    if (board[0][2] == board[1][1] == board[2][0] == X) or (board[0][2] == board[1][1] == board[2][0] == O):
+        return True
 
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == None:
+                return False
 
-    # # check COLUMNS
-    # for i in range(3):
-    #     sum = 0
-    #     for j in range(3):
-    #         if board[j][i] == "X":
-    #             sum += 1
-    #         elif board[j][i] == "O":
-    #             sum -= 1
-
-    #     if sum == 3 or sum == -3:
-    #         return True
-
-    # # check diagonal
-    # if board[0][0] == 
-    # if sum == 3 or sum == -3:
-
-    # # check anti-diagonal    
-    # sum = table_array[0, 2] + table_array[1, 1] + table_array[2, 0]
-    # if sum == 3:
-    #     result = "X wins"
-    #     finished = True
-    #     p1 = (100, 500)
-    #     p2 = (500, 100)
-    # elif sum == -3:
-    #     result = "O wins"
-    #     finished = True
-    #     p1 = (100, 500)
-    #     p2 = (500, 100)
-
-    # for i in range(3):
-    #     for j in range(3):
-    #         if board[i][j] == None:
-    #             return False
-
-    # return True
-    return False
+    return True
 
 
 def utility(board):
@@ -144,7 +115,7 @@ def minimax(board):
     
 
     
-    return move
+    # return move
 
 
 def min_value(board):
